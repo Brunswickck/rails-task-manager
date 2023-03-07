c12feab59dddf6f70fa05fd3bf09ebfcd70c47df
tom burns <tomburns.gj@gmail.com>
2023-03-07 16:23:58 +0000

    removed uneccesssary as: in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create', as: :create_task
+  post 'tasks', to: 'tasks#create'
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update", as: :update_task
-  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
